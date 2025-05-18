128b2d6a0d1fa5b1799798513886373697f5514d
georges ahmed sefiani <georges.sefiani@pixcleaner.com>
2025-05-18 02:21:06 +0200

    feat: allow manifest in browser

--- browser/webpack.config.js
+++ browser/webpack.config.js
@@ -63,7 +63,7 @@ module.exports = {
     resolve: {
       alias: {
           [Path.join(__dirname, '../lib/annotate.js')]: false,
-          [Path.join(__dirname, '../lib/manifest.js')]: false,
+        //   [Path.join(__dirname, '../lib/manifest.js')]: false,
           [Path.join(__dirname, '../lib/trace.js')]: false,
           [Path.join(__dirname, '../lib/types/binary.js')]: false,
           [Path.join(__dirname, '../node_modules/@hapi/tlds/esm/index.js')]: false,
